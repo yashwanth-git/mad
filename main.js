98a5aca39376f9b829674956de463c18852c87f6
Yashwanth Sridharan <karthikyashwanth2009@gmail.com>
2020-09-29 01:35:16 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,87 @@
+//Header on Scroll
+const header = document.querySelector("header");
+
+let lastOffsetY = window.pageYOffset;
+
+window.onscroll = function () {
+    if (window.pageYOffset > lastOffsetY) {
+        header.classList.remove("top");
+        header.classList.add("hide");
+    }
+    else if (window.pageYOffset == 0) {
+        header.classList.add("top");
+    }
+    else {
+        header.classList.remove("top");
+        header.classList.remove("hide");
+    }
+    lastOffsetY = pageYOffset;
+}
+
+//Modal
+const heroBtn = document.querySelector('.Hero__btn');
+const modal = document.querySelector('.Modal');
+const modalClose = document.querySelector('.Modal__close');
+heroBtn.addEventListener('click', () => {
+    modal.classList.add('open');
+})
+modalClose.addEventListener('click', () => {
+    modal.classList.remove('open');
+})
+
+$(document).ready(function () {
+    //Sliding Underline for Tabs
+    $('.Services__list__item').hover(function () {
+        let size = $(this).width();
+        let pos = $(this).position();
+        let marginLeft = parseInt($(this).css('marginLeft')); // To adjust the spacing
+        $('.underline').css('width', size);
+        $('.underline').css('left', pos.left + marginLeft);
+    }, function () {
+        let size = $('.current').width();
+        let pos = $('.current').position();
+        let marginLeft = parseInt($(this).css('marginLeft'));
+        $('.underline').css('width', size);
+        $('.underline').css('left', pos.left + marginLeft);
+    })
+    $('.Services__list__item').on('click', function () {
+        $('.current').removeClass('current');
+        $(this).addClass('current');
+    });
+    //Slider
+    $('#customer__slider').slick({
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplaySpeed: 2000,
+    });
+    //Auto Tabs
+    let total = $('.Team__img').length;
+    let elementIndex = 0;
+
+    let autotabs = setInterval(function () {
+        //Check if current index is last
+        if ($('.Team__img.active').index() == total - 1)
+            elementIndex = 0;
+        else
+            elementIndex++;
+        //Transfer the active class to next element
+        $('.Team__img.active').removeClass('active');
+        $('.Team__member.active').removeClass('active');
+        $('.Team__img').eq(elementIndex).addClass('active');
+        let teamMember = $('.Team__img.active').attr("data-id"); //get corresponding team member id
+        $("#" + teamMember).addClass("active");
+    }, 5000);
+
+    //On click 
+    $('.Team__img').click(function () {
+        clearInterval(autotabs);
+        elementIndex = $(this).index();
+        $('.Team__img.active').removeClass('active');
+        $('.Team__member.active').removeClass('active');
+        $(this).addClass('active');
+        let teamMember = $(this).attr('data-id');
+        $("#" + teamMember).addClass("active");
+    })
+});
